ce24199f636460a23a7bc68627e8723cdaf48c19
sebastian carrero <mugssebastian@gmail.com>
2022-08-10 06:49:32 -0500

    python 10/8/222

---
+++ pythonn/mayusminus.py
@@ -0,0 +1,7 @@
+cadena = input("Digite la cadena de texto: ")
+cadenamayus = cadena.upper()
+print ("cadena en mayuscula " + cadenamayus)
+cadenamin = cadena.lower()
+print ("cadena en minusculas " + cadenamin)
+primeramayus = cadena.title()
+print ("la primera en mayuscula " + primeramayus) 
