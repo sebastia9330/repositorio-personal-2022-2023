55f346965d82a4258280b30c9d1f80f1377154c5
sebastian carrero <mugssebastian@gmail.com>
2022-08-28 14:25:00 -0500

    python 28/8/22

---
+++ pythonn/clases1.py
@@ -0,0 +1,8 @@
+class Ejemplo:
+    def __init__(self, parametro1):
+        self.atributo1 = parametro1
+        self.atributo2 = 100
+
+un_ejemplo = Ejemplo("un_dato")
+
+un_ejemplo.atributo2
--- pythonn/claves.py
+++ pythonn/claves.py
@@ -2,5 +2,4 @@ archivo = open("texto.txt", "a")
 archivo.write("hola me llamo sebastián felipe")
 archivo.close()
 print(archivo.read())
-#hola
-#jajajajajajajaj
+
