ec1d1e150f89192b3cc851984d454c0d1b70636b
sebastian carrero <mugssebastian@gmail.com>
2022-11-10 22:04:37 -0500

    conexion entre servidor y conexion

--- proyectoempresarial/servidor.js
+++ proyectoempresarial/servidor.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const files = require('./conexion')
+
+app.get('/', (req, res) =>{
+    res.end('el servidor se ha iniciado en la parte grafica')
+
+})
 
 /*iniciando configuraciones del servidor*/
 app.listen(5000, function(){
-    console.log('Servidor iniciado')
+    console.log('Servidor iniciado yaaaaaaaaaaaaaaaaaa')
 
 
 })
