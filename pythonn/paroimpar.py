012a8f75e41af2ae0b930db130c907e2a3bbecfa
Sebastian Ortiz <mugssebastian@gmail.com>
2023-01-23 23:45:33 -0500

    even or odd

---
+++ pythonn/paroimpar.py
@@ -0,0 +1,5 @@
+num = int(input("What number are you thinking of?: "))
+if num % 2 == 0:
+    print("It's an even number! Can you add another?")
+else:
+    print("It's an odd number! Can you add another?")
