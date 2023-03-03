28fd9a913bb860c165d237c52c71c98ec866658f
sebastian carrero <mugssebastian@gmail.com>
2023-03-02 23:09:37 -0500

    matrices

---
+++ JavaScript/recordCollection.js
@@ -0,0 +1,29 @@
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+    //console.log(records);
+    console.log(records[id]);
+    prop = records[id]
+    console.log(prop);
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
