a92e47e37f40c47a10db5a54415a870ab5197a1a
Sebastian Ortiz <mugssebastian@gmail.com>
2023-03-21 23:22:37 -0500

    learning javascript

---
+++ proyects web 2023/hola_mundo/app.js
@@ -0,0 +1,3 @@
+/*alert("Hola Mundo")
+alert("Hola sebastian");*/
+console.log("muestra esto en la consola")
---
+++ proyects web 2023/hola_mundo/variables.js
@@ -0,0 +1,11 @@
+var pais = "Colombia";
+pais = "italia"
+contiente = "europa"
+console.log(pais)
+var continente = "America Latina"
+console.log(continente)
+var pais_continente = pais + " " +continente
+console.log(pais_continente)
+
+pais = "italia"
+contiente = "europa"
