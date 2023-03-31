83428b2af777c71798f05871abe649a88b83608d
sebastian carrero <mugssebastian@gmail.com>
2023-03-30 19:55:33 -0500

    learning windows in JS

---
+++ proyects web 2023/alertas/ventanas.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//alerta
+//alert("hola buen dia");
+//alert("adios");
+
+//confirmacion
+var mi_resultado = confirm("estas seguro de querer continuar");
+
+console.log(mi_resultado);
+
+//ingreso datos
+var edad = prompt("¿Que edad tienes?",18);
+console.log(edad);
