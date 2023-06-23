da1c75f727645c734612a5f97e12e1506ecc7fd8
sebastian carrero <mugssebastian@gmail.com>
2023-06-22 23:00:51 -0500

    fetch en el dom

--- proyects web 2023/peticionfetch/app.js
+++ proyects web 2023/peticionfetch/app.js
@@ -2,13 +2,25 @@
 
 //fetch y peticiones a servicios /apis.rest
 
+var div_cartas = document.querySelector('#cartas');
+var div_usuarios = document.querySelector('#usuarios');
+
+
 var usuarios = [];
 
 fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
-        .then(data => data.json())
-        .then(data => {
-            usuarios = data;
+        .then(data => data.json()) //captura de dato y conversion a json
+        .then(users => {
+            usuarios = users.data; //guardar la data en la variable usuarios
             console.log(usuarios)
+
+            /* usuarios.map((user, i) => {
+                let nombre = document.createElement('h3');
+
+                nombre.innerHTML = i + user.name;
+
+                div_cartas.appendChild(nombre)
+            }) */
         })
 
 fetch('https://jsonplaceholder.typicode.com/users')
@@ -16,4 +28,14 @@ fetch('https://jsonplaceholder.typicode.com/users')
         .then(data => {
             usuarios = data;
             console.log(usuarios)
+
+            usuarios.map((user, i) => {
+                let nombre = document.createElement('h3');
+
+                nombre.innerHTML = i + user.name;
+
+                div_usuarios.appendChild(nombre)
+
+                document.querySelector('.loading').style.display = 'none';
+            })
         })
