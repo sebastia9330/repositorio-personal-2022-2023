40c1b4b266ccfdd40754d378d9a14473f9b547a9
sebastian carrero <mugssebastian@gmail.com>
2023-06-26 23:16:45 -0500

    fetch2

--- proyects web 2023/peticionfetch/app.js
+++ proyects web 2023/peticionfetch/app.js
@@ -2,40 +2,24 @@
 
 //fetch y peticiones a servicios /apis.rest
 
-var div_cartas = document.querySelector('#cartas');
+
 var div_usuarios = document.querySelector('#usuarios');
 
 
 var usuarios = [];
+fetch('https://reqres.in/api/users')
+    .then(data => data.json())
+    .then(users => {
+        usuarios = users.data;
+        console.log(usuarios)
 
-fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
-        .then(data => data.json()) //captura de dato y conversion a json
-        .then(users => {
-            usuarios = users.data; //guardar la data en la variable usuarios
-            console.log(usuarios)
+        usuarios.map((user, i) => {
+            let nombre = document.createElement('h3');
 
-            /* usuarios.map((user, i) => {
-                let nombre = document.createElement('h3');
+            nombre.innerHTML = i + ". "+ user.first_name + " " + user.last_name;
 
-                nombre.innerHTML = i + user.name;
+            div_usuarios.appendChild(nombre)
 
-                div_cartas.appendChild(nombre)
-            }) */
+            document.querySelector('.loading').style.display = 'none';
         })
-
-fetch('https://jsonplaceholder.typicode.com/users')
-        .then(data => data.json())
-        .then(data => {
-            usuarios = data;
-            console.log(usuarios)
-
-            usuarios.map((user, i) => {
-                let nombre = document.createElement('h3');
-
-                nombre.innerHTML = i + user.name;
-
-                div_usuarios.appendChild(nombre)
-
-                document.querySelector('.loading').style.display = 'none';
-            })
-        })
+    }); 
