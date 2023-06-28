34180303c8e560252c0a8ac21f46e7d15bc6a993
sebastia9330 <mugssebastian@gmail.com>
2023-06-27 22:35:45 -0500

    fetch final

--- proyects web 2023/peticionfetch/app.js
+++ proyects web 2023/peticionfetch/app.js
@@ -4,22 +4,55 @@
 
 
 var div_usuarios = document.querySelector('#usuarios');
+var div_usuario = document.querySelector('#usuario');
 
 
-var usuarios = [];
-fetch('https://reqres.in/api/users')
-    .then(data => data.json())
-    .then(users => {
-        usuarios = users.data;
-        console.log(usuarios)
+//var usuarios = [];
 
-        usuarios.map((user, i) => {
-            let nombre = document.createElement('h3');
+function getUsuarios(){
+    return fetch('https://reqres.in/api/users');
+}
 
-            nombre.innerHTML = i + ". "+ user.first_name + " " + user.last_name;
+    getUsuarios()    
+        .then(data => data.json())
+        .then(users => {
+        //usuarios = users.data;
+        //console.log(usuarios)
+        listadoUsuarios(users.data)
 
-            div_usuarios.appendChild(nombre)
-
-            document.querySelector('.loading').style.display = 'none';
+            return getUsuario();
         })
-    }); 
+        .then(data => data.json())
+        .then(usuario => {
+            mostrarUsuario(usuario.data);
+        });
+
+function getUsuario(){
+    return fetch('https://reqres.in/api/users/2');
+}
+    
+function listadoUsuarios(usuarios){
+    usuarios.map((user, i) => {
+        let nombre = document.createElement('h3');
+
+        nombre.innerHTML = i + ". "+ user.first_name + " " + user.last_name;
+
+        div_usuarios.appendChild(nombre)
+
+        document.querySelector('.loading').style.display = 'none';
+    });
+};
+
+function mostrarUsuario(user){
+        let nombre = document.createElement('h3');
+        let avatar = document.createElement('img');
+        nombre.innerHTML =  user.first_name + " " + user.last_name;
+
+        avatar.src = user.avatar;
+        avatar.width = "100";
+
+        div_usuario.appendChild(nombre)
+        div_usuario.appendChild(avatar)
+
+        document.querySelector('#usuario .loading').style.display = 'none';
+};
