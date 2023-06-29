601366cdbe0089b14336c872a235c77a2c80eaf2
sebastia9330 <mugssebastian@gmail.com>
2023-06-28 22:44:43 -0500

    fecha javascript

---
+++ proyects web 2023/fechas js/fechas.js
@@ -0,0 +1,15 @@
+var fecha = new Date();
+
+var year = fecha.getFullYear();
+var mes = fecha.getMonth();
+var dia = fecha.getDate();
+var Dia = fecha.getDay();
+
+var textoHora = `
+    El año es: ${year}
+    El mes es: ${mes}
+    El dia es: ${dia}
+    El dia es: ${dia}
+`;
+
+console.log(textoHora);
