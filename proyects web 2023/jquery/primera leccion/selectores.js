bb9b7bf4e2280e5f9b264fdb92a8ebfaa7c40b4f
sebastia9330 <mugssebastian@gmail.com>
2023-06-30 23:35:19 -0500

    selectores en jquery

--- proyects web 2023/jquery/primera leccion/selectores.js
+++ proyects web 2023/jquery/primera leccion/selectores.js
@@ -1,4 +1,31 @@
 $(document).ready(function(){
     //selector de ID
+    var rojo = $("#rojo").css("background","red").css("color","white");
+    var amarillo = $("#amarillo").css("background","yellow")
+    var verde = $("#verde").css("background","green")
+    console.log(rojo)
+
+    //selector de clase
+    var clase = $(".parrafo")
     
-})
+    $(".sinborde").click(function(){
+        console.log("click");
+        $(this).addClass("parrafo")
+    });
+
+    //selectores de etiqueta
+    var parrafos = $("p").css('cursor','pointer');
+
+    parrafos.click(function(){
+        var thiss = $(this);
+
+        if(!thiss.hasClass('grande')){
+            thiss.addClass('grande');
+        }else{
+            thiss.removeClass('grande');
+        }
+    });
+
+    //Selectores de atributo
+    $('[title="inicio"]').css('background','#ccc')
+});
