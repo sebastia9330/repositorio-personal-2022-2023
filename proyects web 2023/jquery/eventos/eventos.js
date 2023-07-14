48d00d73c399d7e94703bf0e0ccf6f0c3acaf394
Sebastian Carrero <mugssebastian@gmail.com>
2023-07-13 23:19:54 -0500

    eventos del mouse con jquery

--- proyects web 2023/jquery/eventos/eventos.js
+++ proyects web 2023/jquery/eventos/eventos.js
@@ -30,4 +30,35 @@ $(document).ready(function(){
     caja.dblclick(function(){
         $(this).css("border", "20px solid red")
     });
+
+    //Focus y blur
+    $('#nombre').focus(function(){
+        $(this).css("border", "5px solid red");
+    });
+
+    $('#nombre').blur(function(){
+        $(this).css("border", "5px solid black");
+        $('#datos').text($(this).val()).show();
+    });
+
+    //Mousedown y mouseup
+    $('#datos').mousedown(function(){
+        $(this).css("border-color", "green");
+
+    });
+
+    $('#datos').mouseup(function(){
+        $(this).css("border-color", "blue");
+    
+    });
+
+    //Mousemove
+    $(document).mousemove(function(){
+        $('body').css('cursor', 'none')
+        console.log("En X: " + event.clientX);
+        console.log("En y: " + event.clientY);
+        $('#sigueme').css("left", event.clientX)
+                    .css("top", event.clientY);
+    })
+
 });
