ec339d7f60e80e12372fa4d1346b41a349a96253
Sebastian Carrero <mugssebastian@gmail.com>
2023-07-17 23:37:11 -0500

    efectos jquery

--- proyects web 2023/jquery/efectos/efectos.js
+++ proyects web 2023/jquery/efectos/efectos.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-    $('h1').css('color','red');
+    //eventos show(mostrar) y hide (ocultar)
+    /* $('#mostrar').hide();
+    
+    $("#mostrar").click(function(){
+        $(this).hide();
+        $('#ocultar').show();
+        $('#caja').show('slow');
+    });
+
+    $("#ocultar").click(function(){
+        $(this).hide();
+        $('#mostrar').show();
+        $('#caja').hide('slow');
+    }) */
+
+    /* $('#mostrar').hide();
+    
+    $("#mostrar").click(function(){
+        $(this).hide();
+        $('#ocultar').show();
+        $('#caja').fadeIn('slow');
+    });
+
+    $("#ocultar").click(function(){
+        $(this).hide();
+        $('#mostrar').show();
+        $('#caja').fadeOut('slow');
+    }) */
+
+    $('#todoenuno').click(function(){
+        $('#caja').toggle();
+    })
 })
