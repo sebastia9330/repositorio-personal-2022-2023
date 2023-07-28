f16aa679d9fa729dfcadf088a6c02de50bde43af
Sebastian Carrero <mugssebastian@gmail.com>
2023-07-27 22:23:13 -0500

    final app de perros

--- proyects web 2023/aplicacion de perros/brain.js
+++ proyects web 2023/aplicacion de perros/brain.js
@@ -2,6 +2,13 @@ const perroActualElement = document.getElementById("perroActual");
 const API = "https://dog.ceo/api/breeds/image/random"
 const spinner = document.getElementById("spinner");
 spinner.classList.add("escondido")
+document.getElementById("like").addEventListener("click",()=> rankingPerros("+"));
+document.getElementById("deslike").addEventListener("click",()=> rankingPerros("-"));
+const likeperro = document.getElementById("likeperro");
+const deslikeperro = document.getElementById("deslikeperro");
+likeperro.classList.add("escondido");
+deslikeperro.classList.add("escondido");
+
 
 document.getElementById("saltar").addEventListener("click",nuevoPerro);
 
@@ -16,6 +23,20 @@ async function nuevoPerro(){
     const res = await fetch(API);
     const resJson = await res.json();
     perroActualElement.src = resJson.message;
+}
+
+function rankingPerros(ranking){
+    const nuevaImagen = document.createElement("img");
+    nuevaImagen.src = perroActualElement.src;
+    if(ranking === "+"){
+        likeperro.appendChild(nuevaImagen);
+    }else{
+        deslikeperro.appendChild(nuevaImagen);
+    }
+    likeperro.classList.toggle("escondido",false)
+    deslikeperro.classList.toggle("escondido",false)
+
+    nuevoPerro();
 }
 
 //Ejecucion
