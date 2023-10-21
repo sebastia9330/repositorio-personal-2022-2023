51a79283e8175bc30a25ab5f1f80bdc1a8fedc49
Sebastian Carrero <mugssebastian@gmail.com>
2023-10-20 20:29:29 -0500

    convertidor de moneda sencillo

---
+++ pythonn/convertidosDolarAPeso.py
@@ -0,0 +1,5 @@
+cantidadDolares = float(input("Por favor, ingresa una cantidad en dolares: "))
+
+cantidadPesos = cantidadDolares * 4232.50
+
+print("Sus dolares en pesos Colombianos son: ",  cantidadPesos)
