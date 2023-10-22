7ae9d279c28aca770ab0c9441359ea11fbe88dc4
Sebastian Carrero <mugssebastian@gmail.com>
2023-10-21 21:55:50 -0500

    juego triqui, parte tres

--- proyects web 2023/triqui/app.js
+++ proyects web 2023/triqui/app.js
@@ -1,31 +1,32 @@
 const x = "X";
 const o = "O";
 let estadoJuego = "P1";
+const modal = document.querySelector("dialog");
+const textoModal = modal.querySelector("h2");
 
 
-
-const cuadrados = document.querySelectorAll(".cuadrado");
+const cuadrados = Array.from(document.querySelectorAll(".cuadrado"));
 
 cuadrados.forEach((cuadrado,i) =>{
     cuadrado.addEventListener("click", ()=>{
         if(estadoJuego === "PAUSA") return;
         if(cuadrado.textContent !== "") return;
         cuadrado.innerText = estadoJuego === "P1" ? x : o;
-        estadoJuego = estadoJuego === "P1" ? "P2" : "P1";
         const posicionGanadora = revisarSiHayGanador();
         if(typeof posicionGanadora === "object"){
             ganar(posicionGanadora);
             return
         }
         if(posicionGanadora === "empate"){
-            console.log("EMPATE")
+            selectmodal("Empate");
         }
+        estadoJuego = estadoJuego === "P1" ? "P2" : "P1";
     })
 })
 
 
 function revisarSiHayGanador(){
-    const tablero = Array.from(cuadrados).map(cuadrado => cuadrado.textContent);
+    const tablero = cuadrados.map(cuadrado => cuadrado.textContent);
     console.log(tablero)
 
     //Revisar horizontales
@@ -63,8 +64,23 @@ function revisarSiHayGanador(){
 
 function ganar(posicionGanadora){
     console.log("Ganador", posicionGanadora);
-    estadoJuego = "PAUSA"
     posicionGanadora.forEach(posicion =>{
         cuadrados[posicion].classList.toggle("ganador",true);
     })
+    selectmodal("El ganador es: "+ estadoJuego);
+    estadoJuego = "PAUSA"
 }
+
+function selectmodal(texto){
+    textoModal.innerText = texto
+    modal.showModal();
+}
+
+modal.querySelector("button").addEventListener("click", ()=>{
+    cuadrados.forEach(cuadrado => {
+        cuadrado.textContent = ""
+        cuadrado.classList.toggle("ganador", false)
+        modal.close();
+        estadoJuego = "P1"
+    });
+});
