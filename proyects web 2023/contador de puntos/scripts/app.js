bc514d621e3306f88c1cfac873f61bac0116a16d
Sebastian Carrero <mugssebastian@gmail.com>
2023-11-07 23:55:00 -0500

    continuando

---
+++ proyects web 2023/contador de puntos/scripts/app.js
@@ -0,0 +1,14 @@
+import {Contador} from "./contador.js";
+
+const p1 = new Contador();
+const p2 = new Contador();
+
+p1.agregar();
+p1.agregar();
+p1.agregar();
+p1.agregar();
+p1.agregar();
+p2.agregar();
+p2.agregar();
+
+
