6d52b62c0d23567bf7e9abac3dd40601af6ce69c
Sebastian Carrero <mugssebastian@gmail.com>
2023-11-11 23:19:25 -0500

    pequeños cambios

--- proyects web 2023/contador de puntos/scripts/app.js
+++ proyects web 2023/contador de puntos/scripts/app.js
@@ -1,7 +1,7 @@
 import {Contador} from "./contador.js";
 
-const p1 = new Contador();
-const p2 = new Contador();
+const p1 = new Contador("JUGADOR 1", document.getElementById("uno"));
+const p2 = new Contador("JUGADOR 2", document.getElementById("dos"));
 
 p1.agregar(3);
 p1.reset();
--- proyects web 2023/contador de puntos/scripts/contador.js
+++ proyects web 2023/contador de puntos/scripts/contador.js
@@ -2,6 +2,11 @@ export class Contador{
     numero = 0;
     equipo;
 
+    constructor(equipo, containerElement, cuentaInicial = 0){
+        this.equipo = equipo
+        containerElement.querySelector("h2").innerText = equipo;
+    }
+
     agregar(cantidad = 1){
         this.numero += cantidad;
         console.log(this.numero);
