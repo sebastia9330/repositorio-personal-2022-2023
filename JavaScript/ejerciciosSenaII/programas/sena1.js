f930ccb5b4ba47f93570b9812824b28d6985f0f9
Sebastian Carrero <mugssebastian@gmail.com>
2023-11-24 23:02:30 -0500

    ejercicio 1

---
+++ JavaScript/ejerciciosSenaII/programas/sena1.js
@@ -0,0 +1,11 @@
+let n1 = parseInt(prompt("Digita el primer numero: "));
+let n2 = parseInt(prompt("Digita el segundo numero: "));
+let n3 = parseInt(prompt("Digita el tercer numero: "));
+
+if(n1 > 0){
+    let produ = n2 * n3;
+    document.write("<h1>El producto de la multiplicacion es: " + produ + "</h1>");
+}else{
+    let sum = n2 + n3;
+    document.write("el resultado de la suma es: " + sum)
+}
