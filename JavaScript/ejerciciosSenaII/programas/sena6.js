4f18df39bf0d199642b044c498038ada14ae2f2d
sebastian carrero <mugssebastian@gmail.com>
2023-11-28 21:24:28 -0500

    sena6 actividad dos

---
+++ JavaScript/ejerciciosSenaII/programas/sena6.js
@@ -0,0 +1,6 @@
+let celcius = parseFloat(prompt("Ingresa los grados celcius: "));
+let Fahre = 0;
+
+Fahre = (celcius * 1.8) + 32
+
+document.write("<h1>La catidad de grados Celcius "+ celcius + " es equivalente a " + Fahre + " Grados Fahrenheit </h1>")
