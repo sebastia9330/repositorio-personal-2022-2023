9f6ddb69d638cd8b16a0fa55dba961266e534839
sebastian carrero <mugssebastian@gmail.com>
2023-12-03 21:35:47 -0500

    prectica numero uno

---
+++ JavaScript/ejerciciospractica/programa/ejercicio1.js
@@ -0,0 +1,8 @@
+let n1 = parseInt(prompt("ingresa el primer nuemero: "));
+let n2 = parseInt(prompt("ingresa el segundo numero: "));
+
+let division = (n1 / n2)/*calculamos la division*/
+let modulo = (n1 % n2)/*calculamos el modulo*/
+
+document.write("El resultado de la division es: " + division);
+document.write("El resultado del modulo es: " + modulo);
