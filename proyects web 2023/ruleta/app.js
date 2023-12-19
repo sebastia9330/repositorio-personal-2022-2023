32b5e3c6a8fa6d8ec70f806c7c245ff6ae70ac4c
sebastian carrero <mugssebastian@gmail.com>
2023-12-18 22:57:11 -0500

    ruleta 1

--- proyects web 2023/ruleta/app.js
+++ proyects web 2023/ruleta/app.js
@@ -1,3 +1,33 @@
+const ruleta = document.getElementById("ruleta");
+
+
 const uno = {
-    nombre: "uno"
+    nombre: "Uno",
+    probabilidad: 20
 }
+
+const dos = {
+    nombre: "Dos",
+    probabilidad: 50
+}
+
+const tres = {
+    nombre: "Tres",
+    probabilidad: 30
+}
+
+let conceptos = [uno, dos, tres];
+
+function ajustarRuleta(){
+    const opcionesContainer = document.createElement("div");
+    opcionesContainer.id = "opcionesContainer"
+    ruleta.appendChild(opcionesContainer);
+    conceptos.forEach(concepto => {
+        //Crear los triangulos de coores
+        const opcionesContainer = document.createElement("div");
+        opcionElement.classList.add("opcion");
+        opcionesContainer.ariaHasPopup(opcionElement)
+    })
+}   
+
+ajustarRuleta();
