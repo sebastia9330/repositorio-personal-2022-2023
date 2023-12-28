5deac8177caf3e01d95e49b151c5f7cf917ace47
Sebastian Carrero <mugssebastian@gmail.com>
2023-12-27 21:59:46 -0500

    fin de año

---
+++ JavaScript/ejerciciospractica/programa/ejercicio8.js
@@ -0,0 +1,9 @@
+let palabra = prompt("Ingresa una frase: ");
+
+let fraseAArray = palabra.split(" ")
+
+document.write("la cantidad de palabras que tiene la frase es: " + fraseAArray.length + "<br>")
+
+document.write("Las palabras son: " + fraseAArray)
+
+
---
+++ proyects web 2023/mecanografia/app.js
@@ -0,0 +1,7 @@
+let empezar = document.getElementById("empezar");
+let barraProgreso = document.createElement("barraprogreso");
+
+
+empezar.addEventListener("click",() =>{
+    console.log("iniciado el test")
+})
