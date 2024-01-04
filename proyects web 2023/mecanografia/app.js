d70cfb79fe08037da582090919edfa100ff9b218
sebastian carrero <mugssebastian@gmail.com>
2024-01-04 17:43:19 -0500

    test de mecanografia 10%

--- proyects web 2023/mecanografia/app.js
+++ proyects web 2023/mecanografia/app.js
@@ -1,7 +1,20 @@
 let empezar = document.getElementById("empezar");
-let barraProgreso = document.createElement("barraprogreso");
+let barraProgreso = document.querySelector("#barraProgreso div");
+
+//variables
+const tiempoJuego = 60;
 
 
 empezar.addEventListener("click",() =>{
     console.log("iniciado el test")
+    barraProgreso.classList.toggle("completarTiempo", true)
+    empezar.classList.toggle("escondido",true)
 })
+
+barraProgreso.addEventListener("animationend", ()=>{
+    console.log("Termino el tiempo")
+})
+
+
+//ejecucion
+document.documentElement.style.setProperty("--tiempo", tiempoJuego + "s")
