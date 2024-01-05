af0268704db098879b6a0987d535abf142b329ed
sebastian carrero <mugssebastian@gmail.com>
2024-01-04 21:28:08 -0500

    mecanografia 20%

--- proyects web 2023/mecanografia/app.js
+++ proyects web 2023/mecanografia/app.js
@@ -1,18 +1,30 @@
-let empezar = document.getElementById("empezar");
-let barraProgreso = document.querySelector("#barraProgreso div");
+const botonEmpezar = document.getElementById("empezar");
+const barraProgreso = document.querySelector("#barraProgreso div");
+const correctasElement = document.querySelector("#correctas span");
+const erroresElement = document.querySelector("#errores span");
+const ppmElement = document.querySelector("#ppm span");
+const final = document.querySelector("#final");
+const botonReiniciar = document.querySelector("#final button")
 
 //variables
-const tiempoJuego = 60;
+const tiempoJuego = 5;
 
-
-empezar.addEventListener("click",() =>{
+//funciones
+function empezar(){
     console.log("iniciado el test")
+    final.classList.toggle("escondido", true)
     barraProgreso.classList.toggle("completarTiempo", true)
-    empezar.classList.toggle("escondido",true)
-})
+    botonEmpezar.classList.toggle("escondido",true)
+}
+
+
+botonEmpezar.addEventListener("click",() => empezar())
+botonReiniciar.addEventListener("click", ()=> empezar())
 
 barraProgreso.addEventListener("animationend", ()=>{
+    final.classList.toggle("escondido",false)
     console.log("Termino el tiempo")
+    barraProgreso.classList.toggle("completarTiempo",false)
 })
 
 
